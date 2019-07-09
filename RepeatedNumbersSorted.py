d3a22b5e2fba0cc7b651cc1a870eaa708632fdb2
SUMIT KUMAR <kumarsumit4961@gmail.com>
2019-07-09 14:02:58 +0530

    Update RepeatedNumbersSorted.py

--- RepeatedNumbersSorted.py
+++ RepeatedNumbersSorted.py
@@ -1,10 +1,15 @@
+n = int(input())
 arr = (input().split())
 a = [int(s) for s in arr]
 a.sort()
-li = []
+li=[]
+
 for i in range(len(a)-1):
     for j in range(i+1,len(a)):
         if (a[i]==a[j]):
             li.append(a[i])
-for k in range(len(li)):
-    print(li[k], end=" ")
+if(len(li)==0):
+    print("unique")
+else:
+    for k in range(len(li)):
+        print(li[k], end=" ")
