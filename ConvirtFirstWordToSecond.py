160b554b20e9e3feaadd70578e93e7ff32ba025a
SUMIT KUMAR <kumarsumit4961@gmail.com>
2019-07-10 14:50:31 +0530

    Add files via upload

---
+++ ConvirtFirstWordToSecond.py
@@ -0,0 +1,17 @@
+var = input().split(" ")
+value1 = list(var[0])
+value1_length = len(value1)
+value2 = list(var[1])
+value2_length = len(value2)
+
+counter = 0
+n = value2_length - value1_length
+
+if(value1_length <= value2_length):
+    for i in range(0,value1_length):
+        if (value1[i] == value2[i]):
+            break
+        else:
+            counter += 1
+
+print(counter+n)
