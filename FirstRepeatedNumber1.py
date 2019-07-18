bda78b1ab6f83ae1862cd1a16c5eeb811ca87dc8
SUMIT KUMAR <kumarsumit4961@gmail.com>
2019-07-19 02:30:43 +0530

    Update and rename FirstRepeatedNumber.py to FirstRepeatedNumber1.py

---
+++ FirstRepeatedNumber1.py
@@ -0,0 +1,9 @@
+wastevalue = int(input())
+arr1 = input().split(" ")
+list2 = [int(s) for s in var1]
+list1 = []
+for i in range(len(list2)-1):
+    for j in range(i+1,len(list2)):
+        if (list2[i] == list2[j]):
+            list1.append(list2[i])
+print(list1[0])
