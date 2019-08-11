c50863c5b4c9e09e614246ab006c764add40b8b4
SUMIT KUMAR <kumarsumit4961@gmail.com>
2019-08-11 20:46:07 +0530

    Add files via upload

---
+++ EqivalentDecimalForRoman.py
@@ -0,0 +1,5 @@
+var = ["I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"]
+var2 = input()
+for i in range(len(var)):
+    if(var[i] == var2):
+        print(i+1)
